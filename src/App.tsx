cb23bd5b74d1311217a2cc9a61eb552d9c0398ff
Phibee <prettyboydeinla11@gmail.com>
2022-01-11 20:50:29 +0400

    added initial resources

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+	return (
+		<>
+			<div className="area">
+				<ul className="circles">
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+				</ul>
+				<div className="glass-container"></div>
+			</div>
+		</>
+	);
+}
+
+export default App;
